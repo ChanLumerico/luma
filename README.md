64649d8a5b83233261f9a7bb2490b35bce3cd877
Chan Lee <greensox284@gmail.com>
2023-12-13 20:54:02 +0900

    *.pyc in .gitignore

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ users gain insights from their machine learning models and communicate results e
 ##
 
 ### Latest Version
-`0.1.1`
+`0.2.0`
 
 ### Dependencies
 `NumPy`, `SciPy`, `Matplotlib`, `Seaborn`
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="luma-ml",
-    version='0.1.1',
+    version='0.2.0',
     author="ChanLumerico",
     author_email="greensox284@gmail.com",
     description="Custom-made powerful and light Python machine learning module",
