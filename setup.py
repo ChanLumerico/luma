a86218a58436148c2bcbf54ee9fe821e6d0b6bb7
Chan Lee <greensox284@gmail.com>
2024-02-05 17:43:21 +0900

    setup.[y

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="luma-ml",
+    version='0.5.9',
+    author="ChanLumerico",
+    author_email="greensox284@gmail.com",
+    description="A Comprehensive Python Module for Machine Learning and Data Science",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChanLumerico/LUMA",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'seaborn'
+    ]
+)
