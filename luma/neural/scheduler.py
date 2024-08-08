8dc5595b3d871706544855a2a4587ca8da8d5325
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-08-08 11:46:18 +0000

    CosAnnealLR

--- luma/neural/scheduler.py
+++ luma/neural/scheduler.py
@@ -1,9 +1,16 @@
 import numpy as np
+import math
 
 from luma.neural.base import Scheduler
 
 
-__all__ = ("StepLR", "ExponentialLR")
+__all__ = (
+    "StepLR",
+    "ExponentialLR",
+    "CosineAnnealingLR",
+)
+
+# TODO: Please add docstrings in the future development.
 
 
 class StepLR(Scheduler):
@@ -17,7 +24,6 @@ class StepLR(Scheduler):
         self.init_lr = init_lr
         self.step_size = step_size
         self.gamma = gamma
-
         self.type_ = "epoch"
 
     @property
@@ -39,14 +45,38 @@ class ExponentialLR(Scheduler):
         super().__init__(init_lr)
         self.init_lr = init_lr
         self.gamma = gamma
-
         self.type_ = "epoch"
 
     @property
     def new_learning_rate(self) -> float:
         epoch_index = self.iter // self.n_iter
-        new_lr = self.init_lr * (self.gamma ** epoch_index)
-        
+        new_lr = self.init_lr * (self.gamma**epoch_index)
+
         self.lr_trace.append(new_lr)
         return new_lr
-    
+
+
+class CosineAnnealingLR(Scheduler):
+    def __init__(
+        self,
+        init_lr: float,
+        T_max: int,
+        eta_min: float = 0,
+    ) -> None:
+        super().__init__(init_lr)
+        self.init_lr = init_lr
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.type_ = "epoch"
+
+    @property
+    def new_learning_rate(self) -> float:
+        epoch_index = self.iter // self.n_iter
+        new_lr = (
+            self.eta_min
+            + (self.init_lr - self.eta_min)
+            * (1 + math.cos(math.pi * epoch_index / self.T_max))
+            / 2
+        )
+        self.lr_trace.append(new_lr)
+        return new_lr
