5d3dbb718031fe6ace37e5c739b7b43db6c17163
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-08-11 17:23:43 +0900

    ResNet_18 docstring

--- luma/neural/model.py
+++ luma/neural/model.py
@@ -1861,7 +1861,68 @@ class InceptionResNet_V2(_models.incep._InceptionRes_V2):
 
 class ResNet_18(_models.resnet._ResNet_18):
     """
-    TODO: Finish this docstring
+    ResNet18 is a 18-layer deep neural network that uses residual blocks 
+    to improve training by learning residuals, helping prevent vanishing 
+    gradients and enabling better performance in image recognition tasks.
+    
+    Structure
+    ---------
+    Input:
+    ```py
+    Tensor[..., 3, 224, 224]
+    ```
+    Residual Blocks:
+    ```py
+    Convolution2D(3, 64, filter_size=7, stride=2) ->  # conv1
+    
+    2x ResNetBlock.Basic(64, 64) ->  # conv2
+    2x ResNetBlock.Basic(128, 128, stride=2) ->  # conv3
+    2x ResNetBlock.Basic(256, 256, stride=2) ->  # conv4
+    2x ResNetBlock.Basic(512, 512, stride=2) ->  # conv5
+    
+    AdaptiveAvgPooling2D((1, 1)) ->  # avg pool
+    ```
+    Fully Connected Layers:
+    ```py
+    Flatten -> Dense(512, 1000)
+    ```
+    Output:
+    ```py
+    Matrix[..., 1000]
+    ```
+    Parameter Size:
+    ```txt
+    11,688,512 weights, 5,800 biases -> 11,694,312 params
+    ```
+    Parameters
+    ----------
+    `activation` : FuncType, default=Activation.ReLU
+        Type of activation function
+    `initializer` : InitStr, default=None
+        Type of weight initializer
+    `out_features` : int, default=1000
+        Number of output features
+    `batch_size` : int, default=100
+        Size of a single mini-batch
+    `n_epochs` : int, default=100
+        Number of epochs for training
+    `valid_size` : float, default=0.1
+        Fractional size of validation set
+    `lambda_` : float, default=0.0
+        L2 regularization strength
+    `early_stopping` : bool, default=False
+        Whether to early-stop the training when the valid score stagnates
+    `patience` : int, default=10
+        Number of epochs to wait until early-stopping
+    `shuffle` : bool, default=True
+        Whethter to shuffle the data at the beginning of every epoch
+
+    References
+    ----------
+    1. He, Kaiming, et al. “Deep Residual Learning for Image Recognition.” 
+    Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition 
+    (CVPR), 2016, pp. 770-778.
+
     """
 
     def __init__(
