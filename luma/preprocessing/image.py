65e48a8ac5f22bea2c22bd80fc9d8539757c765c
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-08-21 11:07:23 +0000

    Resize done

--- luma/preprocessing/image.py
+++ luma/preprocessing/image.py
@@ -1,8 +1,22 @@
 import numpy as np
+from scipy.ndimage import zoom
 from typing import Any, List, Literal
 
 from luma.interface.typing import Tensor
+from luma.interface.exception import UnsupportedParameterError
 from luma.core.super import Transformer
+
+
+__all__ = (
+    "ImageTransformer",
+    "Resize",
+    "CenterCrop",
+    "Normalize",
+    "RandomFlip",
+    "RandomShift",
+    "RandomCrop",
+    "RandomRotate",
+)
 
 
 class ImageTransformer(Transformer, Transformer.Image):
@@ -40,18 +54,50 @@ class Resize(Transformer, Transformer.Image):
     def __init__(
         self,
         size: tuple[int, int],
-        interpolation: Literal[
-            "bilinesr", "bicubic", "gaussian", "nearest"
-        ] = "bilinear",
-        antialias: bool = True,
+        interpolation: Literal["bilinear", "nearest"] = "bilinear",
     ) -> None:
         self.size = size
         self.interpolation = interpolation
-        self.antialias = antialias
-    
+
     @Tensor.force_dim(4)
     def fit_transform(self, X: Tensor) -> Tensor:
-        ...
+        if self.interpolation == "nearest":
+            return self._resize_nearest(X)
+        elif self.interpolation == "bilinear":
+            return self._resize_bilinear(X)
+        else:
+            raise UnsupportedParameterError(self.interpolation)
+
+    def _resize_nearest(self, X: Tensor) -> Tensor:
+        N, C, H, W = X.shape
+        new_H, new_W = self.size
+        scale_H = new_H / H
+        scale_W = new_W / W
+
+        X_resized = np.zeros((N, C, new_H, new_W), dtype=X.dtype)
+        for i in range(N):
+            for j in range(C):
+                X_resized[i, j] = np.repeat(
+                    np.repeat(X[i, j], scale_H, axis=0), scale_W, axis=1
+                )
+
+        return X_resized
+
+    def _resize_bilinear(self, X: Tensor) -> Tensor:
+        N, C, H, W = X.shape
+        new_H, new_W = self.size
+        scale_H = new_H / H
+        scale_W = new_W / W
+
+        X_resized = np.zeros((N, C, new_H, new_W), dtype=X.dtype)
+
+        for i in range(N):
+            for j in range(C):
+                zoom_factor = (scale_H, scale_W)
+                X_resized[i, j] = zoom(X[i, j], zoom_factor, order=1)
+
+        return X_resized
+
 
 class CenterCrop(Transformer, Transformer.Image):
     NotImplemented
@@ -61,13 +107,17 @@ class Normalize(Transformer, Transformer.Image):
     NotImplemented
 
 
-class RandomFlipHorizontal(Transformer, Transformer.Image):
-    NotImplemented
-
-
-class RandomFlipVertical(Transformer, Transformer.Image):
+class RandomFlip(Transformer, Transformer.Image):
     NotImplemented
 
 
 class RandomShift(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class RandomCrop(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class RandomRotate(Transformer, Transformer.Image):
     NotImplemented
