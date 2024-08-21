08141e28bf31f2b2075406616e6d20b96b7e9e75
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-08-21 10:07:52 +0000

    ImageTransformer wip

--- luma/core/super.py
+++ luma/core/super.py
@@ -192,6 +192,24 @@ class Transformer(ModelBase, metaclass=ABCMeta):
             ```
         """
 
+    class Image:
+        """
+        An inner class of `Transformer` dedicated to the image transformations.
+        Usually used for neural networks related to computer vision.
+
+        * `fit_transform` method returns the transformed image set
+
+            ```py
+            def fit_transform(self, X: TensorLike) -> TensorLike
+            ```
+        """
+
+        def fit(self, *args, **kwargs) -> Any:
+            pass
+
+        def transform(self, *args, **kwargs) -> Any:
+            pass
+
     @abstractmethod
     def fit(self, *args) -> Self: ...
 
---
+++ luma/preprocessing/image.py
@@ -0,0 +1,51 @@
+import numpy as np
+from typing import Any
+
+from luma.interface.typing import Tensor
+from luma.core.super import Transformer
+
+
+class ImageTransformer(Transformer, Transformer.Image):
+    def __init__(
+        self,
+        *trans_arr: Transformer,
+        shuffle: bool = False,
+        random_state: int | None = None,
+    ) -> None:
+        self.trans_arr = trans_arr
+        self.shuffle = shuffle
+
+        self.rng_ = np.random.RandomState(random_state)
+
+    @Tensor.force_dim(4)
+    def fit_transform(self, X: Tensor) -> Any:
+        for trans in self.trans_arr:
+            if self.shuffle:
+                X = self.rng_.shuffle(X)
+            X = trans.fit_transform(X)
+
+        return X
+
+
+class Resize(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class Crop(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class Normalize(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class RandomFlipHorizontal(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class RandomFlipVertical(Transformer, Transformer.Image):
+    NotImplemented
+
+
+class RandomShift(Transformer, Transformer.Image):
+    NotImplemented
