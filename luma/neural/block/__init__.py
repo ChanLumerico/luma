3a7d62ae17400d4b8868b82473722baa37f8d22b
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-08-24 11:15:01 +0900

    Rm warning of block/__init__.py

--- luma/neural/block/__init__.py
+++ luma/neural/block/__init__.py
@@ -1,29 +1,3 @@
-"""
-This private module is designated for the pre-definitions of 
-various special-purpose classes utilized within other primary 
-implementations of the project. The scripts and classes within 
-this module are intended exclusively for internal use and are 
-not designed for direct interaction by end users.
-
-Usage
------
-- This module should not be accessed or modified directly.
-- All interactions should be done through the main implementation 
-  modules that import and utilize these classes.
-
-Warnings
---------
-- Direct modifications to this module can lead to unexpected 
-  behavior and are strongly discouraged.
-- This module is not intended for end-user access or usage.
-
-Note
-----
-- For detailed implementation and usage, refer to the main 
-  implementation modules that leverage these pre-defined classes.
-
-"""
-
 from dataclasses import dataclass
 from typing import Literal, Tuple, override
 import numpy as np
