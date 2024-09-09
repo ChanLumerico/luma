dc1d917286683f7cc69cfc79073ddcca6009f981
Chan Lee <robin033@naver.com>
2024-09-09 13:51:04 +0000

    attach_se_block()

--- luma/neural/functional.py
+++ luma/neural/functional.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Optional, Tuple
 from dataclasses import dataclass, asdict
 
 from luma.interface.typing import LayerLike
 from luma.neural.layer import *
+from luma.neural.autoprop import LayerNode, LayerGraph, MergeMode
 
 
 def make_res_layers(
@@ -53,6 +54,30 @@ def make_res_layers(
     return Sequential(*layers), in_channels
 
 
-def attach_se_block(self) -> ...:
-    # Test commit for ChanROKA
-    NotImplemented
+def attach_se_block(
+    layer: LayerLike,
+    se_block: LayerLike,
+    graph_name: str | None = None,
+    pre_build: bool = True,
+    only_graph: bool = False,
+) -> Tuple[str, LayerGraph] | LayerGraph:
+
+    def _get_default_name(layer: LayerLike) -> str:
+        return type(layer).__name__ + "_node"
+
+    root_node = LayerNode(layer, name=_get_default_name(layer))
+    se_node = LayerNode(se_block, name=_get_default_name(se_block))
+    scale_node = LayerNode(Identity(), MergeMode.HADAMARD, name="scale_node")
+
+    graph = LayerGraph(
+        graph={root_node: [se_node, scale_node], se_node: [scale_node]},
+        root=root_node,
+        term=scale_node,
+    )
+    if pre_build:
+        graph.build()
+
+    if graph_name is None:
+        graph_name = type(layer).__name__ + "_SE"
+
+    return graph if only_graph else (graph_name, graph)
