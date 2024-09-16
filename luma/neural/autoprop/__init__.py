df5b80a1caeb0e85a7ad5a39334580cf16939db8
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-09-16 01:27:10 +0000

    Added luma-neural submodule

--- luma/neural/autoprop/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-`AutoProp`
-----------
-Auto propagation system for complex neural networks of Luma Python library.
-
-"""
-
-from .graph import LayerNode, LayerGraph
-from .merge import MergeMode
